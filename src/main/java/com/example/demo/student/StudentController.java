ff116ea4803e17193d795e26e4fb7000707ad997
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2024-05-17 22:48:21 +0530

    Setup connection with PostgreSQL

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,16 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
     @GetMapping
     public List<Student> getStudent() {
-        StudentService studentService = new StudentService();
         return studentService.getStudent();
     }
 
