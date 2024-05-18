07ede9b6688029ddf621b55e16f5a6528102350e
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2024-05-18 20:22:49 +0530

    Setup repository layer

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,22 +10,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudent() {
-        return List.of(
-                new Student(
-                        1L,
-                        "John",
-                        "johnrafael@gmail.com",
-                        LocalDate.of(2000, Month.AUGUST, 8),
-                        24
-                ),
-                new Student(
-                        2L,
-                        "Marie",
-                        "marierogan7@gmail.com",
-                        LocalDate.of(2002, Month.FEBRUARY, 17),
-                        22
-                )
-        );
+        return studentRepository.findAll();
     }
 }
