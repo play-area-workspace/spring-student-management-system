9879ba9a45b04f12f5a9ee485a708cd77760f637
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2024-05-19 12:25:35 +0530

    Setup post method

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -21,4 +21,8 @@ public class StudentService {
     public List<Student> getStudent() {
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
